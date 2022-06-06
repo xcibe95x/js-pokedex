05ffa42f1893e1f1a757ffaf478234c650957a4b
xcibe95x <xcibe95x@gmail.com>
2022-06-06 17:45:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const pokedex = document.querySelector('.poke-container');
+const str1 = "0";
+
+for(i = 0; i <= 150; i++) {
+    fetch('https://pokeapi.co/api/v2/pokemon/' + i)
+    .then(result => result.json())
+    .then(pokemon => insertPokemon(
+        pokemon.sprites.front_default,
+        pokemon.name,
+        pokemon.id,
+        pokemon.types[0].type.name
+    ));
+}
+
+
+function insertPokemon (sprite, name, id, type) {
+    // let parseId = id;
+    // parseId = id.padStart(2, "0");
+    pokedex.innerHTML += `        
+    <div id="dv_${id}" class="poke-card ${type}">
+    <div class="poke-sprite">
+        <img src="${sprite}">
+    </div>
+    <div class="poke-detail">
+        <span class="identifier">#${id.toString().padStart(3, "0")}</span>
+        <p>${name}</p>
+        <p>Type: ${type}</p>
+    </div>
+    </div>
+    </div>
+    `
+    var main = document.getElementById( 'main' );
+[].map.call( main.children, Object ).sort( function ( a, b ) {
+    return +a.id.match( /\d+/ ) - +b.id.match( /\d+/ );
+}).forEach( function ( elem ) {
+    main.appendChild( elem );
+});
+}
+
